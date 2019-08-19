193822ecb12ce5b807ac2cff99be252cfa0f5d42
Rumeet Goradia <rumeet.goradia@gmail.com>
2019-08-18 23:45:43 -0400

    First commit

---
+++ src/components/Cell.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { StyledCell } from './styles/StyledCell'
+import { TETROMINOS } from '../tetrominos'
+
+const Cell = ({ type }) => <StyledCell type={'L'} color={TETROMINOS['L'].color} />
+
+export default Cell;
---
+++ src/components/Display.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyledDisplay } from './styles/StyledDisplay'
+
+const Display = ({ gameOver, text }) => <StyledDisplay gameover={gameOver}>{text}</StyledDisplay>;
+
+export default Display;
