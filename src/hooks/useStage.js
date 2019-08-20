02b22079a812869b97fcc0e1163b55b5f02f07f3
Rumeet Goradia <rumeet.goradia@gmail.com>
2019-08-20 00:52:55 -0400

    Add row clearing

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,8 +4,23 @@ import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+    const sweepRows = newStage => (
+      newStage.reduce((acc, row) => {
+        // Check if you can find a 0 in the row; if not, it's full
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => ++prev);
+          acc.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+          return acc;
+        }
+        acc.push(row);
+        return acc;
+      }, [])
+    )
+
     const updateStage = prevStage => {
       //First flush the stage
       const newStage = prevStage.map(row =>
@@ -26,6 +41,7 @@ export const useStage = (player, resetPlayer) => {
       // Check if collision
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
       return newStage;
     };
@@ -33,5 +49,5 @@ export const useStage = (player, resetPlayer) => {
     setStage(prev => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 };
