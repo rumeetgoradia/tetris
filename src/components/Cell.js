4f862759fb6e906d7ad68aef3bd13aa6d340097c
Rumeet Goradia <rumeet.goradia@gmail.com>
2019-08-20 19:02:07 -0400

    Implement display counters

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,4 +4,4 @@ import { TETROMINOS } from '../tetrominos'
 
 const Cell = ({ type }) => <StyledCell type={type} color={TETROMINOS[type].color} />
 
-export default Cell;
+export default React.memo(Cell);
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -9,6 +9,7 @@ import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 import { usePlayer } from "../hooks/usePlayer";
 import { useStage } from "../hooks/useStage";
 import { useInterval } from "../hooks/useInterval";
+import { useGameStatus } from '../hooks/useGameStatus';
 
 //Components
 import Stage from "./Stage";
@@ -20,9 +21,11 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer);
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared);
 
   console.log("re-render");
+  console.log(dropTime);
 
   const movePlayer = dir => {
     if (!checkCollision(player, stage, { x: dir, y: 0 })) {
@@ -37,9 +40,19 @@ const Tetris = () => {
     setDropTime(1000);
     resetPlayer();
     setGameOver(false);
+    setScore(0);
+    setRows(0);
+    setLevel(0);
   };
 
   const drop = () => {
+    // Increment level after every 10 rows cleared
+    if (rows > (level + 1) * 10) {
+      setLevel(prev => prev + 1);
+      // Increase speed
+      setDropTime(1000 / (level + 1) + 200);
+
+    }
     if (!checkCollision(player, stage, { x: 0, y: 1 })) {
       updatePlayerPos({ x: 0, y: 1, collided: false });
     } else {
@@ -57,7 +70,7 @@ const Tetris = () => {
     if (!gameOver) {
       if (keyCode === 40) {
         console.log('interval on');
-        setDropTime(1000);
+        setDropTime(1000 / (level + 1) + 200);
       }
     }
   }
@@ -97,9 +110,9 @@ const Tetris = () => {
             <Display gameOver={gameOver} text="Game Over" />
           ) : (
               <div>
-                <Display text="Score" />
-                <Display text="Rows" />
-                <Display text="Level" />
+                <Display text={`Score: ${score}`} />
+                <Display text={`Rows: ${rows}`} />
+                <Display text={`Level: ${level}`} />
               </div>
             )}
           <StartButton callback={startGame} />
