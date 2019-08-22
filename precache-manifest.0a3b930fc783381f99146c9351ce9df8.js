b5031f5743aa7c2ae7ac15d7ee4688735be0aa1f
Rumeet Goradia <rumeet.goradia@gmail.com>
2019-08-21 21:26:10 -0400

    Updates

---
+++ precache-manifest.0a3b930fc783381f99146c9351ce9df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bedbb77892cdd8c151dfed83f14b3779",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "fcf1b9cef02b0fef9c05",
+    "url": "/tetris/static/css/main.6b944fa0.chunk.css"
+  },
+  {
+    "revision": "de1d64521edbc81bdd1f",
+    "url": "/tetris/static/js/2.6e0ec2e1.chunk.js"
+  },
+  {
+    "revision": "fcf1b9cef02b0fef9c05",
+    "url": "/tetris/static/js/main.309673ee.chunk.js"
+  },
+  {
+    "revision": "dd696ebab5f20ccab4f7",
+    "url": "/tetris/static/js/runtime~main.a82bdb71.js"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris/static/media/bg.2810fe6b.png"
+  }
+]);
